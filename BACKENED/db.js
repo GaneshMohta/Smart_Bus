595c8731653ff8996d684b3b8342c566f3e96638
Ganesh <ganeshmal.k2022eee@sece.ac.in>
2024-11-18 20:40:58 +0530

    pg-se

--- BACKENED/db.js
+++ BACKENED/db.js
@@ -13,11 +13,11 @@ const connection = mysql.createConnection({
 
 
 const pgconnection = new Client({
-    user: 'postgres',
-    host:'localhost',
-    database:"SmartBus",
-    password:"JaiMahesh#1",
-    port:5433
+    user: process.env.user,
+    host:process.env.host,
+    database:process.env.database,
+    password:process.env.Password,
+    port:process.env.PORT
 })
 
 pgconnection.connect(function(err){
